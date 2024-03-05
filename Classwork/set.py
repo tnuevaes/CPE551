188b7945dceb09d3c1a3cbb24216b8248c285f70
Teddy Nueva Espana <97917873+tnuevaes@users.noreply.github.com>
2024-03-04 19:31:56 -0500

    fix SIR and add set.py

---
+++ Classwork/set.py
@@ -0,0 +1,16 @@
+A = {"a","b","c","d","e"}
+B = {"d","e","f","g","h"}
+
+union = A | B
+print(union)
+
+intersect = A & B
+print(intersect)
+
+diff = A - B
+print(diff)
+
+symm = A ^ B
+print(symm)
+
+  
