5b3ec9b58349f103eccdb3f2379c632e0adfd182
Teddy Nueva Espana <97917873+tnuevaes@users.noreply.github.com>
2024-03-04 19:32:22 -0500

    SIR

--- Classwork/SIR.py
+++ Classwork/SIR.py
@@ -1,38 +1,47 @@
 def enterData():
-    pop = input("Enter population")
-    inf = input("Enter Initially Infected")
-    contact = input("Enter contact rate")
-    rec = input("Enter recovery period")
+    pop = float(input("Enter population"))
+    inf = float(input("Enter Initially Infected"))
+    contact = float(input("Enter contact rate"))
+    rec = float(input("Enter recovery period"))
     return pop, inf, contact, rec
 
 def sim(pop, inf, contact, rec):
     # Calc rec rate
     
-    cr = 1/contact
-    r = rec
+    cr = contact
+    r = 1/rec
     
     # initialize SIR
-    s_val = pop - 1
+    s_val = pop - inf
     i_val = inf
     r_val = 0
     day = 0
     
-    while i_val > 0.5 :
+    print (f"Day {day} : {s_val, i_val, r_val}")
+    day += 1
+    
+    while i_val >= 0.5:
         # calculate s for day
-        s_val = s_val - (cr*i_val*s_val)
+        s_next = s_val - cr * i_val * s_val
         # calculate i for day
-        i_val = i_val + (cr*i_val*s_val) - r*i_val
+        i_next = i_val + cr * i_val * s_val - r * i_val
         # calculate r for day
-        r_val = r_val + (r*i_val)
+        r_next = r_val + r * i_val
         # print the day
-        print ("Day", day ":" format(s_val, .2), format(i_val, .2), format(i_val, .2))
+        print (f"Day {day} : {s_val, i_val, r_val}")
+        s_val = s_next
+        i_val = i_next
+        r_val = r_next
         day += 1
         
         
     
 def main():
+    pop, inf, contact, rec = enterData()
+    sim(pop, inf, contact, rec)
+
     
-    sim()
+
 
 main()
 
