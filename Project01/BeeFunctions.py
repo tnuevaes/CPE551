7e2d4276b5aca57a0086bf5363df8327a5b96b5b
Teddy Nueva Espana <97917873+tnuevaes@users.noreply.github.com>
2024-03-05 18:33:55 -0500

    Create project files and define functions

---
+++ Project01/BeeFunctions.py
@@ -0,0 +1,34 @@
+def loadFlower():
+    # Load flower list
+    return
+    
+def createField(info):
+    # create field
+    # return 2d list
+    return list
+
+def copyField(field):
+    # create copy of the field
+    # Flowers and Pitcher plants are spaces
+    # Spaces are spaces
+    # H is in same position
+    return list
+
+def printField(field):
+    # Print out 2d list in grid format with index numbers along top and left
+    return 
+
+def gameIntro(info):
+    # takes in dictiory of tuples with info
+        # Flower info
+        # # of scout bees
+        # # of worker bees
+        # amount of pollen needed to win
+    # Prints out rules of the game
+    return
+
+def checkSentPosition(Hidden, Visible, x, y, bee, info):
+    # Take in 2d hidden list, 2d visible list, x, y
+    pollen = # amount of pollen harvested
+    return pollen
+
