d0608c5bca8ea4416dd16b7a6f2eedb4106c0901
Teddy Nueva Espana <97917873+tnuevaes@users.noreply.github.com>
2024-04-08 21:00:27 -0400

    Classwork 4.8

--- Classwork/LocalLibraryRedux/cd.py
+++ Classwork/LocalLibraryRedux/cd.py
@@ -1,23 +1,10 @@
-class CD:
-    def __init__(self):
-        self.__musicId = 0
-        self.__albumName = ""
+from item import item
+class CD(item):
+    def __init__(self, id, name, genre, copies):
+        super().__init__(id, name, genre, copies)
+
         self.__artistName = ""
-        self.__genre = ""
         self.__numSongs = 0
-        self.__numCopies = 0
-
-    def getMusicId(self):
-        return self.__musicId
-    
-    def setMusicId(self, musicId):
-        self.__musicId = musicId
-
-    def getAlbumName(self):
-        return self.__albumName
-    
-    def setAlbumName(self, albumName):
-        self.__albumName = albumName
 
     def getArtistName(self):
         return self.__artistName
@@ -25,20 +12,8 @@ class CD:
     def setArtistName(self, artistName):
         self.__artistName = artistName
 
-    def getGenre(self):
-        return self.__genre
-    
-    def setGenre(self, genre):
-        self.__genre = genre
-
     def getNumSongs(self):
         return self.__numSongs
     
     def setNumSongs(self, numSongs):
         self.__numSongs = numSongs
-
-    def getNumCopies(self):
-        return self.__numCopies
-    
-    def setNumCopies(self, numCopies):
-        self.__numCopies = numCopies
--- Classwork/LocalLibraryRedux/library.py
+++ Classwork/LocalLibraryRedux/library.py
@@ -17,7 +17,7 @@ def main():
             #Book code
             #format: [0] = ID, [1] = Type, [2] = Title, [3] = Author, [4] = Genre, [5] = Pages, [6] = copies 
             NewBook = book()
-            NewBook.setBookID(lineList[0])
+            NewBook.setID(lineList[0])
             NewBook.setBookName(lineList[2])
             NewBook.setBookAuthor(lineList[3])
             NewBook.setBookGenre(lineList[4])
@@ -26,12 +26,18 @@ def main():
             #Movie code
             NewMovie = movies()
             NewMovie.setID(lineList[0])
-            NewMovie.setBookName(lineList[2])
-            NewMovie.setBookAuthor(lineList[3])
-            NewMovie.setBookGenre(lineList[4])
-            NewMovie.setPageNum(lineList[5])
+            NewMovie.setName(lineList[2])
+            NewMovie.setGenre(lineList[3])
+            NewMovie.setLength(lineList[4])
+            NewMovie.setCopies(lineList[5])
             
         if lineList[1] == "Music":
             #Music code
             NewMusic = cd()
+            NewMusic.setID(lineList[0])
+            NewMusic.setName(lineList[2])
+            NewMusic.setArtistName(lineList[3])
+            NewMusic.setGenre(lineList[4])
+            NewMusic.setNumSongs(lineList[5])
+            NewMusic.setCopies(lineList[6])
             
